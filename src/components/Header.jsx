3453c89d742772ee6b35adde4e87c864c3940dba
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-02-12 15:36:20 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className=" col-span-3 lg:col-span-2 content-center ... pl-4 pt-4">
       <img
-        src="https://i.postimg.cc/ZYWCxz3n/Logo2.png"
+        src="https://i.postimg.cc/4dkZzX8t/Logo3.png"
         className="h-auto max-w-full object-cover  "
         alt="Flowbite Logo"
       />
