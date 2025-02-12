a6e7c7edaba7c5ca98d18921b41629eac4ddee4a
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-02-12 17:43:11 -0500

    Update Containers.jsx

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -4,9 +4,9 @@ import 'rsuite/dist/rsuite.min.css';
 
 
 const items = [
-    { id: 1 ,descripcion:'Casacas'},
-    { id: 2 ,descripcion:'Pantalones'},
-    { id: 3 ,descripcion:'Polos'},
+    { id: 1 ,descripcion:'CASACAS'},
+    { id: 2 ,descripcion:'PANTALONES'},
+    { id: 3 ,descripcion:'POLOS'},
     // More items...
   ]
 
@@ -15,7 +15,7 @@ export default function Containers() {
  
   <ul role="list" className="space-y-3 pt-2 pl-6 pr-6 pb-10 rounded-3xl ">
     {items.map((item) => (
-      <Button  key={item.id} className='w-full  shadow-xl shadow-blue-400 outline outline-black/5 ' >
+      <Button  key={item.id} className='w-full  shadow-xl shadow-green-300 outline outline-black/5 ' >
       <li key={item.id} className="overflow-hidden ml-4 mr-4 px-4 py-4 
        sm:px-6 text-center  text-4xl font-Londrina-Shadow  drop-shadow-[0_1.2px_4.2px_rgba(0,0,0,0.8)]
        "> {item.descripcion}
