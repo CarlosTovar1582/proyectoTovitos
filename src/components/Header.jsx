f6e06f4520d407bdb30edde9b8300fc367240591
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-02-12 15:36:36 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className=" col-span-3 lg:col-span-2 content-center ... pl-4 pt-4">
+    <div className=" col-span-3 lg:col-span-2 content-center ... pt-4">
       <img
         src="https://i.postimg.cc/4dkZzX8t/Logo3.png"
         className="h-auto max-w-full object-cover  "
