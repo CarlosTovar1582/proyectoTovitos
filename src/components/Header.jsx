5bb38df8ade6dc676b4bf751568278ca124b97e1
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-02-12 16:11:33 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className=" col-span-3 lg:col-span-2 content-center ... pt-4">
+    <div className=" col-span-3 lg:col-span-2 content-center ... pt-6 pl-2 pr-2">
       <img
-        src="https://i.postimg.cc/4dkZzX8t/Logo3.png"
+        src="https://i.postimg.cc/GmDsrmsT/Logo4.png"
         className="h-auto max-w-full object-cover  "
         alt="Flowbite Logo"
       />
