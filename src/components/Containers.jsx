63ad8bba40fb686ed41370c89ca35ada411ebddc
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-02-17 11:52:36 -0500

    Update Containers.jsx

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -15,7 +15,7 @@ export default function Containers() {
  
   <ul role="list" className="space-y-3 pt-2 pl-6 pr-6 pb-10 rounded-3xl ">
     {items.map((item) => (
-      <Button  key={item.id} className='w-full  shadow-xl shadow-green-300 outline outline-black/5 ' >
+      <Button  key={item.id} className='w-full  shadow-xl shadow-blue-600 outline outline-black/5 ' >
       <li key={item.id} className="overflow-hidden ml-4 mr-4 px-4 py-4 
        sm:px-6 text-center  text-4xl font-Londrina-Shadow  drop-shadow-[0_1.2px_4.2px_rgba(0,0,0,0.8)]
        "> {item.descripcion}
