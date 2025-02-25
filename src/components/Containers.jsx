ef8b6165615ca53e9015b14e1b034d1391cdbeb3
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-02-25 13:14:41 -0500

    Update Containers.jsx

--- src/components/Containers.jsx
+++ src/components/Containers.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import { Button } from 'rsuite';
-import 'rsuite/dist/rsuite.min.css';
-
+import React from "react";
+import { Button } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
 
 const items = [
-    { id: 1 ,descripcion:'CASACAS'},
-    { id: 2 ,descripcion:'PANTALONES'},
-    { id: 3 ,descripcion:'POLOS'},
-    // More items...
-  ]
+  { id: 1, descripcion: "CASACAS" },
+  { id: 2, descripcion: "PANTALONES" },
+  { id: 3, descripcion: "CONJUNTOS" },
+  { id: 4, descripcion: "POLOS" },
+  // More items...
+];
 
 export default function Containers() {
   return (
- 
-  <ul role="list" className="space-y-3 pt-2 pl-6 pr-6 pb-10 rounded-3xl ">
-    {items.map((item) => (
-      <Button  key={item.id} className='w-full  shadow-xl shadow-blue-600 outline outline-black/5 ' >
-      <li key={item.id} className="overflow-hidden ml-4 mr-4 px-4 py-4 
-       sm:px-6 text-center  text-4xl font-Londrina-Shadow  drop-shadow-[0_1.2px_4.2px_rgba(0,0,0,0.8)]
-       "> {item.descripcion}
-      </li>
-      </Button>
-    ))}
-  </ul>
-   
-
-  )
+    <div
+      style={{
+        backgroundImage: `url("https://i.postimg.cc/0NLZkWK1/Logo-Fondo.png")`,
+      }}
+    >
+      <ul role="list" className="space-y-6  pl-6 pr-6 pb-32 rounded-3xl ">
+        {items.map((item) => (
+          <Button
+            key={item.id}
+            className="w-full  shadow-xl mt-32 shadow-blue-600 outline outline-black/5 "
+          >
+            <li
+              key={item.id}
+              className="overflow-hidden ml-4 mr-4 px-4 
+             text-center text-black  text-2xl font-semibold  drop-shadow-[0_1.9px_4.2px_rgba(0,0,0,0.8)]
+            "
+            >
+              {" "}
+              {item.descripcion}
+            </li>
+          </Button>
+        ))}
+      </ul>
+    </div>
+  );
 }
