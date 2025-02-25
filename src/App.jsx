f4f4db4f4d50a73c73839f94aa93179769a8a983
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-02-25 13:13:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
   return (
     <>
       <Header />
-      <Division />
+      
       <Containers />   
-      <Division />
+     
       <ContainersSalditos />            
       <Footers/>
       
