b86a4f6e7263d5608bf84acc14d71afb7a3c1892
Kalin <78322484+CarlosTovar1582@users.noreply.github.com>
2025-04-28 17:25:35 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { Divider } from 'rsuite';
-import 'rsuite/dist/rsuite.min.css';
-
-
+import { Divider } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className=" col-span-3 lg:col-span-2 content-center ...  pt-6 pl-2 pr-2">
-      <img
-        src="https://i.postimg.cc/qB3G1NG7/Logo6.png"
-        className="h-auto max-w-full object-cover  "
-        alt="Flowbite Logo"
-      />
-    
-      
+      <Link to={"/Containers/"}>
+        <img
+          src="https://i.postimg.cc/qB3G1NG7/Logo6.png"
+          className="h-auto max-w-full object-cover  "
+          alt="Flowbite Logo"
+        />
+      </Link>
     </div>
   );
 }
